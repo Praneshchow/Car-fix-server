5356b3cf0b9c74ae6dbd14e7d688054bacf6f760
Pranesh98 <praneshchy23@gmail.com>
2024-06-17 04:48:21 +0600

    update operation done

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ Install express, cors, mongodb, dotenv: `npm i express cors mongodb dotenv`
 Run the server: `nodemon index.js`
 
 
+## Detials:
+
+In mongodb, there is one database with two collections. 'CarFix' is the database and it's collections are 'services' and 'bookings'. 
+
+
+I used to update the data using 'patch'. The difference of 'put' and 'patch' are given here, 
+
+PUT: Updates the entire resource, requiring a full representation and overwriting the existing data.
+
+PATCH: Updates only specified fields, making partial changes without affecting other data.
--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 5000;
 
 // middleware. 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());    // for getting the data form web body. 
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2pzzeio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,6 +29,7 @@ async function run() {
     const serviceCollection = client.db('CarFix').collection('services');     // database, collection.    
     const bookingCollection = client.db('CarFix').collection('booking');      // database, collection. 
 
+    // services. 
     app.get('/services', async(req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
@@ -48,7 +50,7 @@ async function run() {
 
     // bookings.
     app.get('/bookings', async(req, res) => {
-      console.log("query email: ", req.query.email);
+      // console.log("query email: ", req.query.email);
       let query = {};
       if (req.query?.email){
         query = {email: req.query.email};
@@ -57,12 +59,38 @@ async function run() {
       res.send(result);
     })
     
+    // insert data. 
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
       console.log(booking);
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
+
+    // update data. 
+    app.patch('/bookings/:id', async(req, res) =>{
+      const id = req.params.id;       // Extracting ID
+      const filter = { _id: new ObjectId(id)};     // creates a filter to find the booking by unique ID
+      
+      const updateBooking = req.body;    // gets the new status data from the request body. 
+      console.log(updateBooking);
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);    // update in the database.
+      res.send(result);       // sends the result of update to the client. 
+    })
+
+    // delete data. 
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -73,6 +101,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
